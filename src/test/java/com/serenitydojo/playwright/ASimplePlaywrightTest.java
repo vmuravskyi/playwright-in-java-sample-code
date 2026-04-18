075b478c74fbe63a6321dc72274962cb75cec84a
v.muravskyi <v.muravskyi@gmail.com>
2026-04-18 19:34:25 +0200

    save

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,15 +1,52 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ASimplePlaywrightTest {
 
+    private Playwright playwright;
+    Browser browser;
+    Page page;
+
+    @BeforeEach
+    void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    void tearDown() {
+        browser.close();
+        playwright.close();
+    }
+
     @Test
     void shouldShowThePageTitle() {
-        // TODO: Write your first playwright test here
+        page.navigate("https://practicesoftwaretesting.com");
+
+        Assertions.assertThat(page.title())
+            .as("")
+            .contains("Practice Software Testing");
     }
+
+    @Test
+    void shouldSearchByKeywork() {
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int countOfMatchingResults = page.locator(".card").count();
+        Assertions.assertThat(countOfMatchingResults)
+            .as("")
+            .isGreaterThan(0);
+    }
+
 }
